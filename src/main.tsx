78117158f83b31ca8fd3cfb1fe9921169fd4b31e
Степан <pokemonivan32@gmail.com>
2024-11-08 13:36:31 +0300

    Сохранение проекта

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {getEditor, setEditorChangeHandler} from "./storage/editor.ts";
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>
-);
+const root = createRoot(document.getElementById('root')!);
+
+function render() {
+    root.render(
+        <StrictMode>
+            <App editor={getEditor()}/>
+        </StrictMode>
+    );
+}
+
+setEditorChangeHandler(render);
+render();
